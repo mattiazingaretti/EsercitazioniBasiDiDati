b0595d6353d21ae0349df80d04da4174888eef6c
mattiazingaretti <zingaretti.mattia@gmail.com>
2020-10-20 21:26:07 +0200

    Done query 5

--- Esercitazione3/Query5.sql
+++ Esercitazione3/Query5.sql
@@ -1,5 +1,4 @@
 
 SELECT  b2.nome ,b1.nome -- ,b2.indirizzo, b1.indirizzo
         FROM bevitore as b2 JOIN bevitore b1 ON b1.indirizzo=b2.indirizzo
-        WHERE b1.indirizzo=b2.indirizzo AND
-             b2.nome!=b1.nome -- avoid pairs (x,x)
+        WHERE b1.indirizzo=b2.indirizzo AND b1.nome > b2.nome
