a2c1c4d612396ff6b0204f2244795b43f4a93d7a
Mattia <zingaretti.mattia@gmail.com>
2020-11-08 10:08:07 +0100

    Done query 8

--- Esercitazione3/Query8.sql
+++ Esercitazione3/Query8.sql
@@ -1,3 +1,16 @@
-Select *
-from (frequenta frec join bevitore bev on frec.bevitore=bev.nome ) join bar ON (bar.nome= frec.bar)
--- where a.volte_a_settimana >= 5 -- and c.indirizzo != b.indirizzo
+
+-- Mia soluzione
+Select bev.nome 
+from (frequenta frec join bevitore bev on frec.bevitore=bev.nome ) 
+join bar ON (bar.nome= frec.bar and bar.indirizzo  != bev.indirizzo  )
+group by bev.nome 
+having SUM(frec.volte_a_settimana)  > 4
+
+
+-- Soluzione del prof 
+-- Malinteso sull'interpretazione della query
+SELECT bevitore.nome
+FROM bevitore JOIN frequenta ON bevitore.nome = frequenta.bevitore
+JOIN bar ON frequenta.bar = bar.nome
+WHERE bevitore.indirizzo != bar.indirizzo AND
+volte_a_settimana > 5
